a8e17f0d949baf327a27bcd69a52fbe0bfd7af1a
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-12-05 13:26:41 +0800

    Revert "refactor: optimize the handling of pred type (#1048)"
    This reverts commit 985aedda32bfd3c3e39d0f6d702483d2ad22a870.

--- stable-diffusion.h
+++ stable-diffusion.h
@@ -65,10 +65,11 @@ enum scheduler_t {
 };
 
 enum prediction_t {
+    DEFAULT_PRED,
     EPS_PRED,
     V_PRED,
     EDM_V_PRED,
-    FLOW_PRED,
+    SD3_FLOW_PRED,
     FLUX_FLOW_PRED,
     FLUX2_FLOW_PRED,
     PREDICTION_COUNT
